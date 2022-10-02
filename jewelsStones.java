2fe3f9a8aafe191c03e00b30908751f5800d25dd
Chaitanya7725 <chaitanya7725@gmail.com>
2022-10-02 19:32:49 -0400

    started working with jewels and stones

---
+++ jewelsStones.java
@@ -0,0 +1,14 @@
+public class jewelsStones {
+
+    public static int numJewelsInStones(String jewels, String stones) {
+        // System.out.println(stones.matches("(.*)"+jewels"+(.*)"));
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        String jewels = "a";
+        String stones = "aAAbbbb";
+        int res = numJewelsInStones(jewels, stones);
+        // System.out.println(res);
+    }
+}
