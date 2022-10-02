b1e8dcb5c7a49c355e14a49e3cfadde07f9a50bf
Chaitanya7725 <chaitanya7725@gmail.com>
2022-10-02 10:07:18 -0400

    undone

---
+++ appendDelete.java
@@ -0,0 +1,20 @@
+/**
+ * appendDelete
+ */
+public class appendDelete {
+
+    public static String appendAndDelete(String s, String t, int k) {
+        int slength = s.length();
+        int tlength = s.length();
+        String result = "";
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "hackerhappy";
+        String t = "hackerrank";
+        int k = 9;
+        appendAndDelete(null, null, 0);
+    }
+
+}
