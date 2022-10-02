a61d53f0126dcedbb003ca147d6f6c920d912e26
Chaitanya7725 <chaitanya7725@gmail.com>
2022-10-02 09:39:16 -0400

    Intro to BigIntegers

---
+++ extraLongFactorials.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+
+public class extraLongFactorials {
+
+    public static void extraLongFactorials(int n) {
+        BigInteger big = new BigInteger("1");
+        for (int i = 2; i <= n; i++) {
+            big = big.multiply(BigInteger.valueOf(i));
+        }
+        System.out.println(big);
+    }
+
+    public static void main(String[] args) {
+        extraLongFactorials(25);
+    }
+}
