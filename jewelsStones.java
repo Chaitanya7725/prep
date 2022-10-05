5271e600f830eef643cae52445cecc51ce70e744
Chaitanya7725 <chaitanya7725@gmail.com>
2022-10-04 23:32:40 -0400

    Check every character using indexOf method

--- jewelsStones.java
+++ jewelsStones.java
@@ -2,13 +2,21 @@ public class jewelsStones {
 
     public static int numJewelsInStones(String jewels, String stones) {
         // System.out.println(stones.matches("(.*)"+jewels"+(.*)"));
-        return 0;
+        int counter = 0;
+        for (int i = 0; i < jewels.length(); i++) {
+            int index = stones.indexOf(String.valueOf(jewels.charAt(i)));
+            while (index != -1) {
+                counter++;
+                index = stones.indexOf(String.valueOf(jewels.charAt(i)), index + 1);
+            }
+        }
+        return counter;
     }
 
     public static void main(String[] args) {
-        String jewels = "a";
-        String stones = "aAAbbbb";
+        String jewels = "aAcCd";
+        String stones = "aAbbCcDDdbabA";
         int res = numJewelsInStones(jewels, stones);
-        // System.out.println(res);
+        System.out.println(res);
     }
 }
