5ada3bef32959eb35d609541c3c0bcac0ac87c0f
Chaitanya7725 <chaitanya7725@gmail.com>
2022-12-28 10:15:28 -0500

    calculate the max product leetcode

---
+++ maxProduct.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class maxProduct {
+
+    public int maxProduct(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++)
+            list.add(nums[i]);
+        Collections.sort(list);
+        return ((list.get(list.size() - 1) - 1) * (list.get(list.size() - 2) - 1));
+    }
+
+    public static void main(String[] args) {
+        // int nums[] = { 1, 2, 5, 5, 4 };
+        int nums[] = { 3, 7 };
+        maxProduct maxProduct = new maxProduct();
+        int result = maxProduct.maxProduct(nums);
+        System.out.println(result);
+    }
+}
