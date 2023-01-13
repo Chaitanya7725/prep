8762f435c3fa4548973ebd568e4fa0d09ee5304a
Chaitanya7725 <chaitanya7725@gmail.com>
2023-01-13 18:48:35 -0500

    Initial LinkedList clone learning from KK

---
+++ LL/Main.java
@@ -0,0 +1,31 @@
+package LL;
+
+public class Main {
+    public static void main(String[] args) {
+        LLC list=new LLC();
+        list.insertFirst(1);
+        list.insertFirst(2);
+        list.insertFirst(3);
+        list.insertFirst(4);
+        list.display();
+        list.insertLast(5);
+        list.display();
+//        list.insertFirst(3);
+//        list.insertFirst(2);
+//        list.insertFirst(8);
+//        list.insertFirst(17);
+//        list.display();
+//        list.insertLast(20);
+//        list.display();
+//        list.insert(100,3);
+//        list.display();
+//        System.out.println(list.deleteFirst());
+//        list.display();
+//        System.out.println(list.deleteLast());
+//        list.display();
+//        System.out.println(list.delete(2));
+//        list.display();
+//        list.update(2,4);
+//        list.display();
+    }
+}
