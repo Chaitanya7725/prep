17c9f103cb1d6d386abae8d752022fe1b8058e3b
Chaitanya7725 <chaitanya7725@gmail.com>
2023-01-22 19:10:38 -0500

    Moving to Arrays package

---
+++ arrays/two_sums.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+
+/**
+ * two_sums
+ * leetcode url: https://leetcode.com/problems/two-sum/
+ */
+public class two_sums {
+
+    public int[] helper(int[] nums, int target) {
+        int[] output = { 0, 1 };
+        if (nums.length == 2)
+            return output;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++)
+            map.put(nums[i],i);
+        for (int i = 0; i < nums.length; i++) {
+            if(map.containsKey(target-nums[i]) && map.get(target-nums[i])!=i){
+                output[0]=i;
+                output[1]=map.get(target-nums[i]);
+                return output;
+            }
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+//         int[] arr = { 2, 11, 7, 15 };
+//         int target = 9;
+
+        int[] arr = { 3, 2, 4 };
+        int target = 6;
+
+//         int[] arr = { 3, 1, 3 };
+//         int target = 6;
+
+        two_sums two_sums = new two_sums();
+        for (int i : two_sums.helper(arr, target)) {
+            System.out.print(i + " ");
+        }
+    }
+}
