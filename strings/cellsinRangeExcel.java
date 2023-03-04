6e37d5e73e876e85127f57be05891ea5ab5c5be6
Chaitanya7725 <chaitanya7725@gmail.com>
2023-03-04 12:19:46 -0500

    Nested for loop and iterate over the characters

--- strings/cellsinRangeExcel.java
+++ strings/cellsinRangeExcel.java
@@ -1,13 +1,12 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+package strings;
+
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class cellsinRangeExcel {
 
     public static List<String> cellsInRange(String s) {
-        List<Character> store = new ArrayList<>();
+        /*List<Character> store = new ArrayList<>();
         List<String> output = new ArrayList<>();
         List<Integer> numbers = new ArrayList<>();
         String[] strings = s.split(":");
@@ -26,14 +25,26 @@ public class cellsinRangeExcel {
                 start++;
             }
         }
-        return output;
+        return output;*/
+        List<String> result= new ArrayList<>();
+        Map<Character,Integer> archive=new HashMap<>();
+        int j=0;
+        for (char i = 'a'; i <= 'z'; i++) {
+            archive.put(i,j);
+            j++;
+        }
+        String [] input = s.split(":");
+        for (char i = input[0].charAt(0); i <= input[1].charAt(0); i++)
+            for (int k = Integer.valueOf(input[0].substring(1)); k <= Integer.valueOf(input[1].substring(1)) ; k++)
+                result.add(i+""+k);
+        return result;
     }
 
     public static void main(String[] args) {
-        // String s = "K1:L2";
-        // String s = "A1:F1";
-        String s = "U7:X9";
+//         String s = "K1:L2";
+         String s = "A1:F1";
+//        String s = "U7:X9";
         List<String> array = cellsInRange(s);
-        System.out.println(Arrays.asList(array));
+        System.out.println(array);
     }
 }
