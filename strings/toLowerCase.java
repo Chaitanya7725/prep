04f4b74dbb81cdf6a91a5ddb9e68b4ace6bea160
Chaitanya7725 <chaitanya7725@gmail.com>
2023-03-06 23:56:20 -0500

    used toLowerCase to lower the case

---
+++ strings/toLowerCase.java
@@ -0,0 +1,15 @@
+package strings;
+
+public class toLowerCase {
+
+//    https://leetcode.com/problems/to-lower-case/
+    public static void main(String[] args) {
+        String s="Hello";
+        System.out.println(toLowerCase(s));
+    }
+
+    private static String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+
+}
