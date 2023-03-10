96bccbfde2a555e0657087db985daf7d18d03987
Chaitanya7725 <chaitanya7725@gmail.com>
2023-03-09 22:55:46 -0500

    Done and used gfg code for pushing 0's to RHS

--- arrays/easy/ApplyOperations.java
+++ arrays/easy/ApplyOperations.java
@@ -1,39 +1,37 @@
 package arrays.easy;
 
 import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.Queue;
 
 public class ApplyOperations {
 
+    // https://www.geeksforgeeks.org/move-zeroes-end-array/
     public static void main(String[] args) {
         int[] nums={1,0,2,0,0,1};
-//        applyOperations(nums);
+        System.out.println(Arrays.toString(applyOperations(nums)));
         int[] nums1={1,2,2,1,1,0};
-//        applyOperations(nums1);
+        System.out.println(Arrays.toString(applyOperations(nums1)));
+        int[] nums2={0,1};
+        System.out.println(Arrays.toString(applyOperations(nums2)));
     }
 
     private static int[] applyOperations(int[] nums) {
-        Queue<Integer> zerosIndex=new PriorityQueue<>();
-        Queue<Integer> nonZeroIndex=new PriorityQueue<>();
-        for (int i = 0; i < nums.length; i++)
-            if(nums[i]==0)
-                zerosIndex.add(i);
-            else
-                nonZeroIndex.add(i);
-        while(!nonZeroIndex.isEmpty()) {
-            int zero = nums[zerosIndex.peek()];
-            int non = nums[nonZeroIndex.peek()];
-            if(non>zero)
-                nonZeroIndex.poll();
-            else{
-                nums[zerosIndex.peek()] = non;
-                nums[nonZeroIndex.peek()] = zero;
-                zerosIndex.poll();
-                nonZeroIndex.poll();
+        for (int i = 1; i < nums.length; i++)
+            if(nums[i]==nums[i-1]){
+                nums[i-1]*=2;
+                nums[i]=0;
             }
-        }
-        return new int[]{};
+        pushZerosToEnd(nums, nums.length);
+        return nums;
+    }
+
+    private static void pushZerosToEnd(int[] nums, int n) {
+        int count = 0;
+        for (int i = 0; i < n; i++)
+            if (nums[i] != 0)
+                nums[count++] = nums[i];
+        while (count < n)
+            nums[count++] = 0;
+
     }
 
 }
