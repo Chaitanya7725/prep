77c744677e927c304e8618b33623bfe6807ae6b2
Chaitanya7725 <chaitanya7725@gmail.com>
2023-03-09 23:42:40 -0500

    condition check and add it

---
+++ arrays/medium/TriangularSumofArray.java
@@ -0,0 +1,29 @@
+package arrays.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TriangularSumofArray {
+
+    public static void main(String[] args) {
+        int[] nums={1,2,3,4,5};
+        System.out.println(triangularSum(nums));//8
+
+        int[] nums1={5};
+        System.out.println(triangularSum(nums1));//5
+    }
+
+    private static int triangularSum(int[] nums) {
+        for (int i = nums.length; i >=0 ; i--) {
+            for (int j = 1; j < i; j++) {
+                if((nums[j]+nums[j-1])>9)
+                    nums[j-1]=(nums[j]+nums[j-1])%10;
+                else
+                    nums[j-1]=nums[j]+nums[j-1];
+            }
+        }
+        return nums[0];
+    }
+
+}
