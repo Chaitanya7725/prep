d99f4dba33beaec4cf852ac0e640fd0c34472333
Chaitanya7725 <chaitanya7725@gmail.com>
2023-04-06 20:46:22 -0400

    adding comments

--- Assessments/Eyenuk/ColorbyNumbers.java
+++ Assessments/Eyenuk/ColorbyNumbers.java
@@ -20,23 +20,30 @@ public class ColorbyNumbers {
     }
 
     private static String ColorByNumbers(String[] input){
+        // mentioned units in the code
         int usage=5;
         int dull=30;
         int empty=60;
         String result="";
+        // Count for tracking
         int dullCount=0;
         int emptyCount=0;
+        //Two maps for counting overall and for each String
         Map<Character,Integer> recGlobal=new HashMap<>();
         Map<Character,Integer> recLocal = null;
         for (String in : input) {
             recLocal=new HashMap<>();
+            // Counting
             for (char ch:in.toCharArray()) {
                 recLocal.put(ch,recLocal.getOrDefault(ch,0)+1);
                 recGlobal.put(ch,recGlobal.getOrDefault(ch,0)+1);
             }
             int count=0;
+            // Getting count
             for (char c:recLocal.keySet())
                 count+=recLocal.get(c);
+
+            // Calculating for the dull sharpener and emptying it
             for (char c:recGlobal.keySet()){
                 if(recGlobal.get(c)>10) {
                     dullCount=recGlobal.get(c)/10;
