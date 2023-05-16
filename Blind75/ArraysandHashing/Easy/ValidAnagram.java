04f171f86e559b3d9560ddea954ade91f5c6afb6
Chaitanya7725 <chaitanya7725@gmail.com>
2023-05-16 11:42:22 -0400

    easy problem from blind75: used streams as in Mutable Reduction Operation

---
+++ Blind75/ArraysandHashing/Easy/ValidAnagram.java
@@ -0,0 +1,28 @@
+package Blind75.ArraysandHashing.Easy;
+
+public class ValidAnagram {
+
+    public static void main(String[] args) {
+        String s="anagram";
+        String t="nagaram";
+        System.out.println(isAnagram(s , t));
+
+        s="rat";
+        t="car";
+        System.out.println(isAnagram(s , t));
+    }
+
+    private static boolean isAnagram(String s, String t) {
+        // https://www.scaler.com/topics/sort-a-string-in-java/
+        return s.chars().sorted().collect(
+                StringBuilder::new,
+                StringBuilder::appendCodePoint,
+                StringBuilder::append
+        ).toString().equals(t.chars().sorted().collect(
+                StringBuilder::new,
+                StringBuilder::appendCodePoint,
+                StringBuilder::append
+        ).toString());
+    }
+
+}
