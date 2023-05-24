9f5bee2cdf14316bc9d2bcd42c87b53142f5a093
Chaitanya7725 <chaitanya7725@gmail.com>
2023-05-23 20:57:08 -0400

    used string builder to reverse the string

---
+++ Blind75/TwoPointers/ValidPalindrome.java
@@ -0,0 +1,20 @@
+package Blind75.TwoPointers;
+
+public class ValidPalindrome {
+
+    public static void main(String[] args) {
+        String s="A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s));
+
+        String s1="0P";
+        System.out.println(isPalindrome(s1));
+    }
+
+    private static boolean isPalindrome(String s) {
+        s=s.toLowerCase().replaceAll("[^a-z0-9]","");
+        StringBuilder sb=new StringBuilder();
+        sb.append(s);
+        return s.equals(String.valueOf(sb.reverse()));
+    }
+
+}
