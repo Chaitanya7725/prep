7a93e4f8181e23f691e5150af59d508587b173f1
Chaitanya7725 <chaitanya7725@gmail.com>
2023-05-26 21:24:28 -0400

    used 2 pointer approach with O(n) time complexity

--- Blind75/TwoPointers/Medium/ContainerWithMostWater.java
+++ Blind75/TwoPointers/Medium/ContainerWithMostWater.java
@@ -8,13 +8,25 @@ public class ContainerWithMostWater {
     }
 
     private static int maxArea(int[] height) {
+//        int result=0;
+//        int area=0;
+//        for (int l = 0; l < height.length; l++) {
+//            for (int r = l + 1; r < height.length ; r++) {
+//                area=(r-l)*Math.min(height[l],height[r]);
+//                result=Math.max(area,result);
+//            }
+//        }
+//        return result;
         int result=0;
-        int area=0;
-        for (int l = 0; l < height.length; l++) {
-            for (int r = l + 1; r < height.length ; r++) {
-                area=(r-l)*Math.min(height[l],height[r]);
-                result=Math.max(area,result);
-            }
+        int left=0;
+        int right=height.length-1;
+        for (int i = 0; i < height.length; i++) {
+            int area=(right - left) * Math.min(height[left],height[right]);
+            result=Math.max(area,result);
+            if(height[left]<height[right])
+                left++;
+            else
+                right--;
         }
         return result;
     }
