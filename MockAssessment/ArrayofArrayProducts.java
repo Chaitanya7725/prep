bea9c2824759282eb961ada03395352b30830093
Chaitanya7725 <chaitanya7725@gmail.com>
2023-06-28 17:31:36 -0400

    updated version having only a single Array consumption

--- MockAssessment/ArrayofArrayProducts.java
+++ MockAssessment/ArrayofArrayProducts.java
@@ -8,30 +8,29 @@ public class ArrayofArrayProducts {
 
         int[] arr1={2,3,0,982,10};//[0,0,58920,0,0]
         System.out.println(Arrays.toString(arrayOfArrayProducts(arr1)));
-
+//
         int[] arr2={1};
         System.out.println(Arrays.toString(arrayOfArrayProducts(arr2)));
     }
-
     private static int[] arrayOfArrayProducts(int[] arr) {
-        if(arr.length==1) return new int[]{};
-        int total=1;
+        if(arr.length<=1) return new int[]{};
+
         int [] output=new int[arr.length];
-        int zeroCount=0;
+        for (int i = 0; i < output.length; i++)
+            output[i]=1;
 
-        for(int i=0;i<arr.length;i++)
-            if(arr[i]!=0)
-                total*=arr[i];
-            else
-                zeroCount++;
+        int prefix=1;
+        int postfix=1;
 
-        for(int i=0;i<arr.length;i++)
-            if(arr[i]==0)
-                output[i]=total;
-            else if(arr[i]!=0 && zeroCount==1)
-                output[i]=0;
-            else
-                output[i]=total/arr[i];
+        for (int i = 0; i < output.length; i++) {
+            output[i]=prefix;
+            prefix*= arr[i];
+        }
+
+        for (int i = output.length-1; i >=0; i--) {
+            output[i]*=postfix;
+            postfix*=arr[i];
+        }
 
         return output;
     }
