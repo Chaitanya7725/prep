a986b7d6f34a84a04c547e9ff9a9525f213b9499
Chaitanya7725 <chaitanya7725@gmail.com>
2023-07-12 23:26:44 -0400

    https://leetcode.com/problems/merge-intervals Enhanced the code using neet

--- NeetcodeAll/MergeIntervals/MergeIntervals.java
+++ NeetcodeAll/MergeIntervals/MergeIntervals.java
@@ -1,9 +1,6 @@
 package MergeIntervals;
 
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.*;
 
 public class MergeIntervals {
 
@@ -16,54 +13,27 @@ public class MergeIntervals {
     }
 
     private static int[][] merge(int [][] intervals) {
-        if(intervals.length<2)
-            return intervals;
-        LinkedList<ArrayList<Integer>> arrLL = new LinkedList<>();
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        LinkedList<ArrayList<Integer>> check = new LinkedList<>();
+        check.add(new ArrayList<>(){{
+                add(0,intervals[0][0]);
+                add(1,intervals[0][1]);
+        }});
         for (int i = 1; i < intervals.length; i++) {
-            int first0=intervals[i-1][0];
-            int first1=intervals[i-1][1];
-            int second0=intervals[i][0];
-            int second1=intervals[i][1];
-            if(i==1){
-                if(first1 >= second0){
-                    arrLL.add(new ArrayList<>(){
-                        {
-                            add(0,first0);
-                            add(1,second1);
-                        }
-                    });
-                }else{
-                    arrLL.add(new ArrayList<>(){
-                        {
-                            add(0,first0);
-                            add(1,first1);
-                        }
-                    });
-                    arrLL.add(new ArrayList<>(){
-                        {
-                            add(0,second0);
-                            add(1,second1);
-                        }
-                    });
-                }
+            if(check.getLast().get(1)>=intervals[i][0]){
+                check.getLast().set(1,Integer.max(check.getLast().get(1),intervals[i][1]));
             }else{
-                if(arrLL.getLast().get(1) >= second0){
-                    arrLL.getLast().add(1,second1);
-                    arrLL.getLast().remove(arrLL.getLast().size()-1);
-                }else{
-                    arrLL.add(new ArrayList<>(){
-                        {
-                            add(0,Integer.min(first0,first1));
-                            add(1,Integer.max(second0,second1));
-                        }
-                    });
-                }
+                int finalI = i;
+                check.add(new ArrayList<>(){{
+                    add(0,intervals[finalI][0]);
+                    add(1,intervals[finalI][1]);
+                }});
             }
         }
-        int [][] res=new int[arrLL.size()][2];
+        int [][]res=new int[check.size()][2];
         for (int i = 0; i < res.length; i++) {
-            res[i][0]=arrLL.get(i).get(0);
-            res[i][1]=arrLL.get(i).get(1);
+            res[i][0]=check.get(i).get(0);
+            res[i][1]=check.get(i).get(1);
         }
         return res;
     }
